43ecec8918985e13a7c25330ef48d0c6721f74d5
mironex <miroslaw.sarna@gmail.com>
2025-03-06 15:34:26 +0100

    add logging request detail in stub server (#60)
    * add logging request detail in stub server
    
    * update after remark on PR (skip Authorization header and limit for body)
    
    * update test threshold value

--- cmd/mcvs-stub-server/main.go
+++ cmd/mcvs-stub-server/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 	"sync"
 )
+
+const maxBodySizeBytes = 1024 * 10 //
 
 func main() {
 	h := newHandler()
@@ -76,6 +82,8 @@ func (h *handler) configure(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *handler) catchAll(w http.ResponseWriter, r *http.Request) {
+	log.Default().Println(logRequestContext(r))
+
 	response, exists := h.endpoints[r.URL.Path]
 	if !exists {
 		http.NotFound(w, r)
@@ -93,6 +101,46 @@ func (h *handler) catchAll(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Default().Println("Failed to write response:", err)
 	}
+}
+
+func logRequestContext(r *http.Request) string {
+	var requestInfo strings.Builder
+
+	requestInfo.WriteString(fmt.Sprintf("Request Method: %s\n", r.Method))
+	requestInfo.WriteString(fmt.Sprintf("Absolute URL: %s\n", r.URL.String()))
+	requestInfo.WriteString(fmt.Sprintf("Absolute Path: %s\n", r.URL.Path))
+	requestInfo.WriteString(fmt.Sprintf("Host: %s\n", r.Host))
+	requestInfo.WriteString(fmt.Sprintf("Remote Address: %s\n", r.RemoteAddr))
+
+	requestInfo.WriteString("Headers:\n")
+	for name, values := range r.Header {
+		if strings.EqualFold(name, "Authorization") {
+			requestInfo.WriteString(fmt.Sprintf("  %s: *****\n", name))
+			continue
+		}
+		for _, value := range values {
+			requestInfo.WriteString(fmt.Sprintf("  %s: %s\n", name, value))
+		}
+	}
+
+	requestInfo.WriteString("Query Parameters:\n")
+	for name, values := range r.URL.Query() {
+		for _, value := range values {
+			requestInfo.WriteString(fmt.Sprintf("  %s: %s\n", name, value))
+		}
+	}
+
+	if r.Body != nil {
+		bodyBytes, err := io.ReadAll(io.LimitReader(r.Body, maxBodySizeBytes))
+		if err != nil {
+			requestInfo.WriteString(fmt.Sprintf("Error reading request body: %v\n", err))
+		} else {
+			requestInfo.WriteString(fmt.Sprintf("Body Content:\n%s\n", string(bodyBytes)))
+			r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+		}
+	}
+
+	return requestInfo.String()
 }
 
 func (h *handler) list(w http.ResponseWriter, r *http.Request) {
